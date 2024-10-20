dc1620a234885ee9e3845ca7f9e8bfd3b662481c
DeeEmDy <dy-lanarias@hotmail.comcls>
2024-10-20 12:59:22 -0600

    Changes lab 4 done

--- pet-app/src/app/app.routes.ts
+++ pet-app/src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 
 export const routes: Routes = [
+  {
+    path: '', //Para indicar que cuando este en la raiz de la URL, redirija a la ruta dashboard. localhost:4200/dashboard = esto debe cargar el dashboard.
+    redirectTo: 'dashboard', //Redirige a la ruta dashboard para cargarlo en la pagina principal o raiz de la URL.
+    pathMatch: 'full' //Para indicar que la redireccion es completa.
+  },
   { path: 'users', component: UserComponent }, //Ruta que se va a mostrar en la URL y el componente que se va a cargar.
+  { path: 'dashboard', component: DashboardComponent} //Ruta que se va a mostrar en la URL y el componente que se va a cargar.
 ];
--- pet-app/src/app/components/sidebar/sidebar.component.ts
+++ pet-app/src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { RouteInfo } from '../../route-info';
+import { CommonModule } from '@angular/common';
+
+//Definición de una constante para manejar las rutas de los componentes de la aplicación.
+export const ADMIN_ROUTES: RouteInfo[] = [
+  { path: 'dashboard', title: 'Dashboard', icon: 'fa-solid fa-calendar text-blue', class: '' }, //Ruta del componente Dashboard con su respectivo ícono y título.
+  { path: 'users', title: 'Users', icon: 'fa-solid fa-user text-blue', class: '' }, //Ruta del componente Users con su respectivo ícono y título.
+];
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule], //Uso del RouterModule para la navegación y el CommonModule para las directivas estructurales
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
 
+//Clase SidebarComponent que contiene la lógica para el componente sidebar
+export class SidebarComponent {
+  public menuItems: any[] = []; //Arreglo de objetos que contiene las rutas de los componentes de la aplicación
+  public isCollapsed = true;
+
+  constructor(private router: Router){}
+
+  //Método que se ejecuta al iniciar el componente.
+  ngOnInit() {
+    this.menuItems = ADMIN_ROUTES.filter(menuItem => menuItem); //Se filtran las rutas de los componentes de la aplicación.
+
+    this.router.events.subscribe((event) => {
+      this.isCollapsed = true; //Se utiliza para saber si el navbar está colapsado o no.
+    });
+  }
 }
